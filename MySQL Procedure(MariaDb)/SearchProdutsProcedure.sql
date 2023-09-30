909ec60253e1d4d19a11823a6bd8d6bf58bb460e
Vimal1020 <vimalmessi107@gmail.com>
2023-09-30 15:25:35 +0530

    proc

--- MySQL Procedure(MariaDb)/SearchProdutsProcedure.sql
+++ MySQL Procedure(MariaDb)/SearchProdutsProcedure.sql
@@ -1,9 +1,5 @@
--- Change the delimiter
 DELIMITER //
-
--- If the stored procedure exists, drop it
 DROP PROCEDURE IF EXISTS SearchProducts;
-
 CREATE PROCEDURE SearchProducts(
     IN p_ProductName VARCHAR(255),
     IN p_Size VARCHAR(255),
@@ -13,34 +9,31 @@ CREATE PROCEDURE SearchProducts(
     IN p_Conjunction VARCHAR(3)
 )
 BEGIN
-    DECLARE sqlQuery VARCHAR(1000);
-
-    SET sqlQuery = 'SELECT * FROM tbl_Product WHERE 1=1 ';
+    SET @sqlQuery = 'SELECT * FROM tbl_Product WHERE 1=1 ';
 
     IF p_ProductName IS NOT NULL AND p_ProductName <> '' THEN
-        SET sqlQuery = CONCAT(sqlQuery, p_Conjunction, ' ProductName LIKE ''%', p_ProductName, '%'' ');
+        SET @sqlQuery = CONCAT(@sqlQuery, p_Conjunction, ' ProductName LIKE ''%', p_ProductName, '%'' ');
     END IF;
 
     IF p_Size IS NOT NULL AND p_Size <> '' THEN
-        SET sqlQuery = CONCAT(sqlQuery, p_Conjunction, ' Size LIKE ''%', p_Size, '%'' ');
+        SET @sqlQuery = CONCAT(@sqlQuery, p_Conjunction, ' Size LIKE ''%', p_Size, '%'' ');
     END IF;
 
     IF p_Price IS NOT NULL THEN
-        SET sqlQuery = CONCAT(sqlQuery, p_Conjunction, ' Price = ', p_Price, ' ');
+        SET @sqlQuery = CONCAT(@sqlQuery, p_Conjunction, ' Price = ', p_Price, ' ');
     END IF;
 
     IF p_MfgDate IS NOT NULL THEN
-        SET sqlQuery = CONCAT(sqlQuery, p_Conjunction, ' MfgDate = ''', p_MfgDate, ''' ');
+        SET @sqlQuery = CONCAT(@sqlQuery, p_Conjunction, ' MfgDate = ''', p_MfgDate, ''' ');
     END IF;
 
     IF p_Category IS NOT NULL AND p_Category <> '' THEN
-        SET sqlQuery = CONCAT(sqlQuery, p_Conjunction, ' Category LIKE ''%', p_Category, '%'' ');
+        SET @sqlQuery = CONCAT(@sqlQuery, p_Conjunction, ' Category LIKE ''%', p_Category, '%'' ');
     END IF;
 
-    PREPARE stmt FROM sqlQuery;
+    PREPARE stmt FROM @sqlQuery;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
 END //
 
--- Change the delimiter back to semicolon
 DELIMITER ;
